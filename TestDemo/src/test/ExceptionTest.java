fd2b4621271895d05b66d1637e2fed2375c47308
shashipr <shashipr@shashipr-w8.cybage.com>
2017-05-04 12:25:38 +0530

    Producer_Consumer & a few more

--- TestDemo/src/test/ExceptionTest.java
+++ TestDemo/src/test/ExceptionTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.nio.file.AccessDeniedException;
+
 public class ExceptionTest {
 
 	@SuppressWarnings("finally")
@@ -7,18 +9,34 @@ public class ExceptionTest {
 		// TODO Auto-generated method stub
 		int val=0;
 		try {
-			if (val==0)
-				throw new ClassCastException();
+			ExceptionTest et = new ExceptionTest();
+			et.NewException();
 			System.out.println("in try");
+			if (val==0)
+				throw new ArithmeticException();
+			
 		}
 		catch (Exception e){
 			System.out.println("in catch");
+			e.printStackTrace();
 			throw new Exception();
 		}
 		finally {
 			System.out.println("IN FINALLY ");
 			throw new NumberFormatException();
 		}
-		
+
+	}
+	
+	public void NewException() throws AccessDeniedException {
+		System.out.println("in method");
+		throw new AccessDeniedException("say what");
+		/*try {
+			System.out.println("in method");
+			throw new AccessDeniedException("say what");
+		} catch (AccessDeniedException e) {
+			// TODO Auto-generated catch block
+			System.out.println("in catchie");
+		}*/
 	}
 }
--- TestDemo/src/test/SetTest.java
+++ TestDemo/src/test/SetTest.java
@@ -7,14 +7,19 @@ public class SetTest {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void main(String []args) {
 		
-		TreeSet sety = new TreeSet();
+		Set<String> sety = new HashSet<String>();
 		sety.add("2");
-		sety.add(1);
+		//sety.add(1);
+		sety.add(new Integer(1).toString());
 		sety.add("3");
 		
-			sety.forEach(System.out::println);
+		sety.forEach(System.out::println);
 
 		
+		Set sety2 = new HashSet();
+		sety2.add("qwerty");
+		sety2.add(new Integer(12));
 		
+		sety2.forEach(System.out::println);
 	}
 }
